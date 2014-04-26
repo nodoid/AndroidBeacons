efa1b4063e875b966dc52b0f541e1c37164f0f4c
Paul Johnson <paul@snuberry-software.co.uk>
2014-04-26 05:18:54 +0100

    Fixes so it works as it should

--- TagBeacons/MainActivity.cs
+++ TagBeacons/MainActivity.cs
@@ -8,20 +8,21 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Bluetooth;
+using Android.Support.V4.App;
 using RadiusNetworks.IBeaconAndroid;
 
 namespace TagBeacons
 {
-    [Activity(Label = "TagBeacons", MainLauncher = true)]
+    [Activity(Label = "TagBeacons", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleTask)]
     public class MainActivity : Activity, IBeaconConsumer
     {
-        private readonly string TagName = "TagPoints";
+        private const string TagName = "TagPoints";
         private bool AppPaused;
         private IBeaconManager beaconManager;
         private MonitorNotifier monitorNotifier;
         private RangeNotifier rangeNotifier;
         private Region monitorRegion, rangeRegion;
-        private TextView textUUID, textProximity, textDistance, textMajor, textMinor, textMessage, textNumberSeen, textStatus;
+        private TextView textUUID, textProximity, textDistance, textMajor, textMinor, textMessage, textNumberSeen, textStatus, textInRange;
         private DBManager dbm;
 
         public MainActivity()
@@ -31,7 +32,7 @@ namespace TagBeacons
             rangeNotifier = new RangeNotifier();
             monitorRegion = new Region(TagName, null, null, null);
             rangeRegion = new Region(TagName, null, null, null);
-            DBManager dbm = new DBManager();
+            dbm = new DBManager();
             dbm.SetupDB();
         }
 
@@ -47,11 +48,14 @@ namespace TagBeacons
             textMessage = FindViewById<TextView>(Resource.Id.textMessage);
             textNumberSeen = FindViewById<TextView>(Resource.Id.textNumberSeen);
             textStatus = FindViewById<TextView>(Resource.Id.textStatus);
+            textInRange = FindViewById<TextView>(Resource.Id.textInRange);
+
+            textUUID.Text = textProximity.Text = textMajor.Text = textMinor.Text = textMessage.Text = "";
 
             var btAdapter = BluetoothAdapter.DefaultAdapter;
             if (btAdapter.IsEnabled)
             {
-                textStatus.SetTextColor(Android.Graphics.Color.Blue);
+                textStatus.SetTextColor(Android.Graphics.Color.AliceBlue);
                 textStatus.Text = "Searching";
             }
             else
@@ -64,6 +68,7 @@ namespace TagBeacons
 
             monitorNotifier.EnterRegionComplete += EnterRegion;
             monitorNotifier.ExitRegionComplete += ExitRegion;
+            rangeNotifier.DidRangeBeaconsInRegionComplete += RangingBeaconsInRegion;
         }
 
         protected override void OnResume()
@@ -95,19 +100,81 @@ namespace TagBeacons
 
         private void EnterRegion(object s, MonitorEventArgs e)
         {
-            textStatus.SetTextColor(Android.Graphics.Color.Green);
-            textStatus.Text = "Beacon detected";
+            RunOnUiThread(() =>
+            {
+                textStatus.SetTextColor(Android.Graphics.Color.Green);
+                textStatus.Text = "Beacon detected";
+            });
         }
 
         private void ExitRegion(object s, MonitorEventArgs e)
         {
-            textStatus.SetTextColor(Android.Graphics.Color.Blue);
-            textStatus.Text = "Searching";
+            RunOnUiThread(() =>
+            {
+                textStatus.SetTextColor(Android.Graphics.Color.Blue);
+                textStatus.Text = "Searching";
+            });
         }
 
         private void RangingBeaconsInRegion(object s, RangeEventArgs e)
         {
+            RunOnUiThread(() => textInRange.Text = e.TagBeacons.Count.ToString());
+            var beac = e.TagBeacons.FirstOrDefault();
+            if (beac != null)
+            {
+                string prox = "";
+                RunOnUiThread(() => textUUID.Text = beac.ProximityUuid);
+                switch ((ProximityType)beac.Proximity)
+                {
+                    case ProximityType.Immediate:
+                        prox = "Very close";
+                        break;
+                    case ProximityType.Near:
+                        prox = "Close";
+                        break;
+                    case ProximityType.Far:
+                        prox = "Not too close";
+                        break;
+                }
+                RunOnUiThread(() =>
+                {
+                    textProximity.Text = prox;
+                    textMajor.Text = beac.Major.ToString();
+                    textMinor.Text = beac.Minor.ToString();
+                    textDistance.Text = beac.Accuracy.ToString();
+                });
+                var exist = dbm.GetSingleBeacon(beac.ProximityUuid);
 
+                var beacon = new Beacon
+                {
+                    Proximity = beac.Proximity,
+                    DeviceUUID = beac.ProximityUuid,
+                    SignalStrength = beac.Rssi,
+                    Major = beac.Major,
+                    Minor = beac.Minor,
+                    DeviceDistance = beac.Accuracy,
+                    WelcomeMessage = "",
+                    LastSeen = DateTime.Now,
+                };
+
+                if (exist == null)
+                    beacon.FirstSeen = DateTime.Now;
+
+                dbm.InsertOrUpdateBeacon(beacon);
+                var total = dbm.GetListOfBeacons().Select(t => t.DeviceUUID).Distinct().ToList().Count;
+                RunOnUiThread(() => textNumberSeen.Text = total.ToString());
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            monitorNotifier.EnterRegionComplete -= EnterRegion;
+            monitorNotifier.ExitRegionComplete -= ExitRegion;
+            rangeNotifier.DidRangeBeaconsInRegionComplete -= RangingBeaconsInRegion;
+            beaconManager.StopMonitoringBeaconsInRegion(monitorRegion);
+            beaconManager.StopRangingBeaconsInRegion(rangeRegion);
+            beaconManager.UnBind(this);
         }
     }
 }
